dd326c94934f7a424a4a4cd1903b9ad1f9d1931e
Ryan Noronha <ryannor56a@gmail.com>
2018-03-11 19:45:40 +0530

    Added comments and some extra functionality

--- mini.py
+++ mini.py
@@ -1,14 +1,16 @@
 import argparse
 
+# Initialise the parser
 parser = argparse.ArgumentParser()
 
-option_help = "Options: 1. Calculator, 2. Check if number is armstrong or not, 3. Display Fibonacci series"
-
-parser.add_argument("-o", "--option", type = int, choices = [1, 2, 3], help = option_help)
+# Add options 
+parser.add_argument("-o", "--option", type = int, choices = [1, 2, 3], help = "Options: 1. Calculator, 2. Check if number is armstrong or not, 3. Display Fibonacci series")
 parser.add_argument("-i", "--inputs", type = int, help = "Inputs for the  selected option.")
 
+# To get the arguments as variables
 args = parser.parse_args()
 
+# Driving the menu
 if args.option == 2:
 	number = args.inputs
 	onum = args.inputs
@@ -16,16 +18,23 @@ if args.option == 2:
 	temp = 0
 	size = 0
 
+	# To calculate the number of digits of the entered number
 	while number != 0:
 		size += 1
 		number //= 10
 
 	number = args.inputs
 
+	# Main code to check if number is Armstrong or not
 	while number != 0:
 		rem = number % 10
 		temp += (rem ** size)
 		number //= 10
 
+	# To tell if Armstrong
 	if temp == onum:
 		print("{} is an Armstrong number".format(args.inputs))
+
+	# Number is not Armstrong
+	else:
+		print("{} is not an Armstrong number".format(args.inputs))
