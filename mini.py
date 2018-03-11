28f9687627a938847c3eee209f794013922473b7
Ryan Noronha <ryannor56a@gmail.com>
2018-03-11 21:04:00 +0530

    Project complete, awaiting affirmation from members

--- mini.py
+++ mini.py
@@ -3,9 +3,11 @@ import argparse
 # Initialise the parser
 parser = argparse.ArgumentParser()
 
-# Add options 
-parser.add_argument("-o", "--option", type = int, choices = [1, 2, 3], help = "Options: 1. Calculator, 2. Check if number is armstrong or not, 3. Display Fibonacci series")
-parser.add_argument("-i", "--inputs", type = int, help = "Inputs for the  selected option.")
+# Add postional and optional arguments
+parser.add_argument("option", type = int, choices = [1, 2, 3], help = "Options: 1. Calculator, 2. Check if number is armstrong or not, 3. Display Fibonacci series")
+parser.add_argument("input", type = int, help = "Input for the  selected option.")
+parser.add_argument("-c", "--calc_option", type = int, choices = [1, 2, 3, 4], help = "Calculator options: 1. Add, 2. Subtract, 3. Multiply, 4. Divide")
+parser.add_argument("-i2", "--input2", type = int, help = "Use for inputing the second number to the calculator.")
 
 # To get the arguments as variables
 args = parser.parse_args()
@@ -14,8 +16,8 @@ args = parser.parse_args()
 
 # Check if entered number is Armstrong or not
 if args.option == 2:
-	number = args.inputs
-	onum = args.inputs
+	number = args.input
+	onum = args.input
 
 	temp = 0
 	size = 0
@@ -25,7 +27,7 @@ if args.option == 2:
 		size += 1
 		number //= 10
 
-	number = args.inputs
+	number = args.input
 
 	# Main code to check if number is Armstrong or not
 	while number != 0:
@@ -35,20 +37,20 @@ if args.option == 2:
 
 	# To tell if Armstrong
 	if temp == onum:
-		print("{} is an Armstrong number".format(args.inputs))
+		print("{} is an Armstrong number".format(args.input))
 
 	# Number is not Armstrong
 	else:
-		print("{} is not an Armstrong number".format(args.inputs))
+		print("{} is not an Armstrong number".format(args.input))
 
 # Print Fibonacci series upto entered number
 elif args.option == 3:
-	count = args.inputs
+	count = args.input
 	n1 = 0
 	n2 = 1
 	i = 0
 
-	print("Fibonacci series for {} terms:".format(args.inputs))
+	print("Fibonacci series upto {} terms:".format(args.input))
 
 	# Calculate the series
 	while i < count:
@@ -60,4 +62,31 @@ elif args.option == 3:
 		i += 1
 
 	print()
+
+
+else:
+	# Calculate the sum of the numbers
+	if args.calc_option == 1:
+		result = args.input + args.input2
+
+		print("The sum of {} and {} is {}".format(args.input, args.input2, result))
+
+	# Calculate the difference between the numbers
+	elif args.calc_option == 2:
+		result = args.input - args.input2
+
+		print("The difference between {} and {} is {}".format(args.input, args.input2, result))
+
+	# Calculate the product of the  numbers
+	elif args.calc_option == 3:
+		result = args.input * args.input2
+
+		print("The product of {} and {} is {}".format(args.input, args.input2, result))
+
+	# Calculate the results of division
+	else:
+		quo = args.input // args.input2
+		rem = args.input % args.input2
+
+		print("{} divided by {} gives: Quotient = {} and Remainder = {}".format(args.input, args.input2, quo, rem))
 
