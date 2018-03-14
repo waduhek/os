48ba91396011099fa4b4bd9dbdd55e1ee450cb13
Ryan Noronha <ryannor56a@gmail.com>
2018-03-14 20:41:15 +0530

    Added comments

--- mini2.py
+++ mini2.py
@@ -3,8 +3,10 @@
 import os 
 import argparse
 
+# Initialise the parser
 parser = argparse.ArgumentParser()
 
+# Add positional and optional arguments
 parser.add_argument("option", type = int, choices = [1, 2, 3, 4, 5, 6], help = "Options: 1. Add, 2. Subtract, 3. Multiply, 4. Divide, 5. Modulo, 6. Factorial")
 parser.add_argument("input", type = int, help = "Compulsory input for the calculator.")
 parser.add_argument("input2", type = int, help ="Second input for the calculator (If not needed put a 0)")
@@ -12,6 +14,7 @@ parser.add_argument("-i3", type = int, help = "Use if 2 operations are to be per
 parser.add_argument("-i4", type = int, help = "Use if 2 operations are to be performed at the same time.(Do not use for factorial)")
 
 
+# Utility functions for driving the calculator
 def add(a, b):
 	return a + b
 
@@ -35,10 +38,13 @@ def factorial(a):
 
 	return result
 
+# To get arguments as variables
 args = parser.parse_args()
 
+# Create a child process
 pid = os.fork()
 
+# Summing the numbers entered
 if args.option == 1:
 	if pid == 0:
 		print("The sum of {} and {} is {}".format(args.input, args.input2, add(args.input, args.input2)))
@@ -57,6 +63,7 @@ if args.option == 1:
 		except TypeError:
 			os.wait()
 
+# Finding the difference betwwen the entered numbers
 elif args.option == 2:
 	pass
 
