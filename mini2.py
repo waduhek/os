1398e079ec985e1d165fae5b05f3b2ed5798b725
Ryan Noronha <ryannor56a@gmail.com>
2018-03-15 19:09:03 +0530

    Added the rest of the program and modified functionality

--- mini2.py
+++ mini2.py
@@ -44,26 +44,86 @@ args = parser.parse_args()
 # Create a child process
 pid = os.fork()
 
-# Summing the numbers entered
-if args.option == 1:
-	if pid == 0:
-		print("The sum of {} and {} is {}".format(args.input, args.input2, add(args.input, args.input2)))
+# Child process could not be created
+if pid < 0:
+	print("Child process could not be created!")
 
-		os._exit(0)
-
-	elif pid < 0:
-		print("Child process could not be created!")
-
-	else:
-		try:
+# Child process was successfully created
+elif pid == 0:
+	# Check if optional inputs were provided or not
+	try:
+		# Perform addtion
+		if args.option == 1:
 			print("The sum of {} and {} is {}".format(args.i3, args.i4, add(args.i3, args.i4)))
 
-			os.wait()
+			os._exit(0)
 
-		except TypeError:
-			os.wait()
+		# Perform subtraction
+		elif args.option == 2:
+			print("The difference between {} and {} is {}".format(args.i3, args.i4, subtract(args.i3, args.i4)))
 
-# Finding the difference betwwen the entered numbers
-elif args.option == 2:
-	pass
+			os._exit(0)
+
+		# Perform multiplication
+		elif args.option == 3:
+			print("The product of {} and {} is {}".format(args.i3, args.i4, multiply(args.i3, args.i4)))
+
+			os._exit(0)
+
+		# Perform division
+		elif args.option == 4:
+			print("The result of dividing {} by {} is {}".format(args.i3, args.i4, multiply(args.i3, args.i4)))
+
+			os._exit(0)
+
+		# Perform modulo
+		elif args.option == 5:
+			print("The remainder of dividing {} by {} is {}".format(args.i3, args.i4, modulo(args.i3, args.i4)))
+
+			os._exit(0)
+
+		# Find the factorial
+		elif args.option == 5:
+			print("The factorial of {} is {}".format(args.input, factorial(args.input)))
+
+			os._exit(0)
+
+	
+	# Handle the exception raised if the optional inputs are not present
+	except TypeError:
+		os._exit(0)
+
+# Still in parent process
+else:
+	# If 
+	if args.option == 1:
+		print("The sum of {} and {} is {}".format(args.input, args.input2, add(args.input, args.input2)))
+
+		os.wait()
+
+	elif args.option == 2:
+		print("The difference between {} and {} is {}".format(args.i3, args.i4, subtract(args.input, args.input2)))
+
+		os.wait()
+
+	elif args.option == 3:
+		print("The product of {} and {} is {}".format(args.i3, args.i4, multiply(args.input, args.input2)))
+
+		os.wait()
+
+	elif args.option == 4:
+		print("The result of dividing {} by {} is {}".format(args.i3, args.i4, multiply(args.input, args.input2)))
+
+		os.wait()
+
+	elif args.option == 5:
+		print("The remainder of dividing {} by {} is {}".format(args.i3, args.i4, modulo(args.input, args.input2)))
+
+		os.wait()
+
+	elif args.option == 5:
+		print("The factorial of {} is {}".format(args.input, factorial(args.input2)))
+
+		os.wait()
+
 
