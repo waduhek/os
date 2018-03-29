0d98bb180c5e429771a36b23025d5077429db01c
Ryan Noronha <ryannor56a@gmail.com>
2018-03-29 19:55:10 +0530

    Removed the 2 caluclations at a time functionality

--- mini2.py
+++ mini2.py
@@ -54,69 +54,61 @@ pid = os.fork()
 if pid < 0:
 	print("Child process could not be created!")
 
-
 # Child process was successfully created
 if pid == 0 and k == 0:
 	print("\nThe returned value from fork is:", pid)
 	print("The process ID is:", os.getpid())
 
-	# Check if optional inputs were provided or not
-	try:
-		# Perform addtion
-		if args.option == 1:
-			print("The sum of {} and {} is {}.".format(args.input, args.input2, add(args.input, args.input2)))
-			os._exit(0)
+	# Perform addtion
+	if args.option == 1:
+		print("The sum of {} and {} is {}.".format(args.input, args.input2, add(args.input, args.input2)))
+		os._exit(0)
 
-		# Perform subtraction
-		elif args.option == 2:
-			print("The difference between {} and {} is {}.".format(args.input, args.input2, subtract(args.input, args.input2)))
-
-			os._exit(0)
-
-		# Perform multiplication
-		elif args.option == 3:
-			print("The product of {} and {} is {}.".format(args.input, args.input2, multiply(args.input, args.input2)))
-
-			os._exit(0)
-
-		# Perform division
-		elif args.option == 4:
-			# Check for division by 0
-			try:
-				print("The result of dividing {} by {} is {}.".format(args.input, args.input2, divide(args.input, args.input2)))
-
-				os._exit(0)
-
-			# Handle ZeroDivisionError if 0 is the divisor
-			except ArithmeticError:
-				print("Illegal division!")
-
-				os._exit(0)
-
-		# Perform modulo
-		elif args.option == 5:
-			try:
-				print("The remainder of dividing {} by {} is {}.".format(args.input, args.input2, modulo(args.input, args.input2)))
-
-				os._exit(0)
-
-			except ArithmeticError:
-				print("Illegal operation!")
-
-				os._exit(0)
-
-		# Find the factorial
-		elif args.option == 6:
-			print("The factorial of {} is {}.".format(args.input, factorial(args.input)))
-
-			os._exit(0)
-
-	# Handle the exception raised if the optional inputs are not present
-	# Exception raised will be NoneTypeError
-	except TypeError:
-		print("Since no inputs are provided, operation was performed by the parent process.")
+	# Perform subtraction
+	elif args.option == 2:
+		print("The difference between {} and {} is {}.".format(args.input, args.input2, subtract(args.input, args.input2)))
 
 		os._exit(0)
+
+	# Perform multiplication
+	elif args.option == 3:
+		print("The product of {} and {} is {}.".format(args.input, args.input2, multiply(args.input, args.input2)))
+
+		os._exit(0)
+
+	# Perform division
+	elif args.option == 4:
+		# Check for division by 0
+		try:
+			print("The result of dividing {} by {} is {}.".format(args.input, args.input2, divide(args.input, args.input2)))
+
+			os._exit(0)
+
+		# Handle ZeroDivisionError if 0 is the divisor
+		except ArithmeticError:
+			print("Illegal division!")
+
+			os._exit(0)
+
+	# Perform modulo
+	elif args.option == 5:
+		try:
+			print("The remainder of dividing {} by {} is {}.".format(args.input, args.input2, modulo(args.input, args.input2)))
+
+			os._exit(0)
+
+		except ArithmeticError:
+			print("Illegal operation!")
+
+			os._exit(0)
+
+	# Find the factorial
+	elif args.option == 6:
+		print("The factorial of {} is {}.".format(args.input, factorial(args.input)))
+
+		os._exit(0)
+
+	print()
 
 # Still in parent process
 # Perform the same tasks as above
